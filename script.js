c226054eeacbdfe4854d5d7dab9e1f344860b0ed
Iwant2dye8976 <114306575+Iwant2dye8976@users.noreply.github.com>
2024-04-27 16:38:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -7,14 +7,12 @@ function Decode()
             var hexSubstring = inputText.value.substr(i, 2);
             if (hexSubstring.length !== 2) {
                 alert("Độ dài chuỗi không hợp lệ.");
-                throw new Error('Chuỗi không hợp lệ để chuyển đổi thành số thập lục phân.');
             }
             var v = parseInt(hexSubstring, 16);
             if (!!v) {
                 str += String.fromCharCode(v);
             } else {
                 alert("Không thể chuyển đổi chuỗi.");
-                throw new Error('Không thể chuyển đổi thành số thập lục phân: ' + hexSubstring);
             }
             document.getElementById("outputField").value = str;
         } catch (error) {
